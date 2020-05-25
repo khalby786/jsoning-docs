ee7f8cdbb3fc4195dc8b35aa2eaf92f3d2292af6
Khaleel Gibran <khalby786@gmail.com>
2020-05-25 08:26:03 +0530

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,7 @@ module.exports = {
   theme: 'yuu',
   themeConfig: {
     repo: "khalby786/jsoning",
+    logo: "https://cdn.glitch.com/c393fad9-338a-43b4-9a2f-8ba07e26d39d%2Fjsoning.png?v=1589190601684",
     // docsDir: 'guide',
     sidebarDepth: 3,
     editLinks: true,
