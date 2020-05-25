20e343e2aa5a4457134e9846f3f12c083179c6ba
Khaleel Gibran <khalby786@gmail.com>
2020-05-25 08:36:04 +0530

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,9 @@ module.exports = {
   base: '/',
   title: 'Jsoning',
   description: "A simple key-value JSON-based persistent lightweight database.",
+  head: [
+    ['link', { rel: 'icon', href: 'https://cdn.glitch.com/a1686874-cbbf-4ca9-b412-cd53a73b9ceb%2Fjsoning_square.png?v=1590375603069' }]
+  ],
   theme: 'yuu',
   themeConfig: {
     repo: "khalby786/jsoning",
