c916d6f82567d2aa9677cefa0241b488fc633f27
Khaleel Gibran <khalby786@gmail.com>
2020-06-16 07:30:28 +0000

    Update sidebar.js

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -8,7 +8,7 @@ module.exports = {
       title: 'Basic Usage',
       children: [
         '/basic-operations/creating-a-new-json-database',
-        '/basic-operations/creating-a-new-element
+        '/basic-operations/creating-a-new-element'
       ]
     }
   ]
