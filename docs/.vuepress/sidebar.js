15036c58168733fc94a4cacad9c884a3fc96ad27
Khaleel Gibran <khalby786@gmail.com>
2020-06-16 07:15:33 +0000

    Create sidebar.js

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = {
+  '/': [
+    {
+      title: 'Home',
+      children: ['/']
+    },
+    {
+      title: 'Basic Usage',
+      children: [
+        '/basic-operations/creating-a-new-json-database'
+      ]
+    }
+  ]
+};
